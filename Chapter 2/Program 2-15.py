6c3a3fe99d10a09aaee3f301761ae5e6e69a8fe1
Jose M. Puebla <joey.manuel132@gmail.com>
2024-09-27 23:23:27 -0600

    Third Push
    Chapter 2, Exercises 12 through 18

---
+++ Chapter 2/Program 2-15.py
@@ -0,0 +1,16 @@
+# sale_price.py
+
+# This program gets an item's original price and
+# calculates its sale price, with a 20% discount.
+
+# Get the item's orignal price.
+original_price = float(input("Enter the item's irignal price: "))
+
+# Calculate the amount of the discount.
+discount = original_price * 0.2
+
+# Calculate the sale price.
+sale_price = original_price - discount
+
+# Display the sale price
+print('The sale price is ', sale_price)
