18b11c73fce2e1b6445aad2d6c5273326333cfd9
Jose M. Puebla <joey.manuel132@gmail.com>
2024-11-17 18:04:05 -0700

    Tenth Push
    Chapter 4, Programs 12-15

---
+++ Chapter 4/Program 4-13.py
@@ -0,0 +1,28 @@
+# property_tax.py
+
+# This program displays property taxes.
+
+TAX_FACTOR = 0.0065         # Represents the tax factor.
+
+# Get the first lot number
+print('Enter the property lot number')
+print('or enter 0 to end.')
+lot = int(input('Lot number: '))
+
+# Continue processing as long as the user
+# Does not enter a lot number 0.
+
+while lot != 0:
+    # Get the property value.
+    value = float(input('Enter the property value: '))
+
+    # Calculate the property's tax.
+    tax = value * TAX_FACTOR
+
+    # Display the tax.
+    print('Property tax: $', format(tax, ',.2f'), sep='')
+
+    # Get the next lot number
+    print('Enter the next lot number')
+    print('enter 0 to end.')
+    lot = int(input('Lot number: '))
